09450ab61033109092f566fe9550f502d15ed397
Tuan Quan Lam <a1919043@adelaide.edu.au>
2024-07-24 21:02:31 +0930

    Some changes

--- function-3-2.cpp
+++ function-3-2.cpp
@@ -11,8 +11,8 @@ int median_array(int array[], int n){
     //sort the array
     int tempt = 0; 
 
-    for(int i = 0; i < n; i++){
-        for(int j = 0; j < n - i; j++){
+    for(int i = 0; i < n-1; i++){
+        for(int j = 0; j < n - i - 1; j++){
             if(array[j] > array [j+1]){
                 tempt = array[j];
                 array[j] = array[j+1]; 
