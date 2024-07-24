24403a989c94dd0b3938eb7005537ed2b0ca2b36
Tuan Quan Lam <a1919043@adelaide.edu.au>
2024-07-24 21:07:29 +0930

    Some more changes on Q-3-2

--- main-3-2.cpp
+++ main-3-2.cpp
@@ -3,7 +3,7 @@
 int median_array(int array[], int n);
 
 int main(){
-    int array[] = {1,2,3,4,5,6}; 
+    int array[] = {1,2}; 
     std:: cout << "Result:" << median_array(array, sizeof(array)/sizeof(int)) << std:: endl; 
     return 0; 
 
