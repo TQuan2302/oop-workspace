15ce8bb6b31e73c7a6e61f9e040e1697b59bb525
Tuan Quan Lam <a1919043@adelaide.edu.au>
2024-08-06 13:35:33 +0930

    Some Changes

--- practice4.cpp
+++ practice4.cpp
@@ -11,9 +11,3 @@ double arrayMax(double* array, int n){
     }
     return max; 
 }
-
-int main(){
-    double array[] = {1.2,3.22,5.8,10.9};
-    double number = arrayMax(array, sizeof(array)/sizeof(double)); 
-    cout << number << endl; 
-}
