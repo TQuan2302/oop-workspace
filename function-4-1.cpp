d626e1e6a0af761f4017efe8679d072a29433c41
Tuan Quan Lam <a1919043@adelaide.edu.au>
2024-08-12 13:10:51 +0930

    Finish function-4-1.cpp

--- function-4-1.cpp
+++ function-4-1.cpp
@@ -12,26 +12,103 @@ int findSum(int* array, int size){
 
 
 
-int secondSmallestSum(int *numbers,int length){
-    int smallest = -10000; 
-    int subLength = 0; 
+int findMinOfSubArray(int *numbers,int length){
+    int min = 100000; 
+    int subLength = 1; 
     
     
-    for(int i = 0; i<length; i++){
-        int* subArray = new int(subLength); 
-        for(int j = 0; j < i; j++){
+    for(int i = 0; i < length; i++){
+        //Create the size for subArray
+        int* subArray = new int[subLength]; 
+
+        //Add every element of the original array into sub array: {1},{1,2},{1,2,3}, {1,2,3,4}
+        for(int j = 0; j < i+1; j++){
             subArray[j] = numbers[j]; 
-            cout << "SubArray: " << subArray[i] << "Array: " << numbers[i] << endl; 
-            subLength++; 
         }
+
         int tempt_sum = findSum(subArray, subLength);
-        if(smallest < tempt_sum){
-            smallest  = tempt_sum; 
+
+        //Find Min among subArrays:
+        for(int i = 0; i<subLength; i++){
+            if(min > tempt_sum){
+                min = tempt_sum; 
+            }
         }
+        
+        subLength++; 
         delete[] subArray; 
-        cout << endl; 
        
     }
-    return smallest;
+    return min; 
 }
+
+
+int secondSmallestSum(int *numbers,int length){
+    int minArray[100] = {}; 
+    int counter = 0; 
+    int sizeOfSubArray = length; 
+    int min = 100000; 
+    int secondMin = 100000; 
+
+    //Find the smallest number of sum among subarrays:
+    for(int i = 0; i<length; i++){
+        int tempt_min = findMinOfSubArray(&(numbers[i]), sizeOfSubArray);
+        minArray[i] = tempt_min; 
+        counter++;
+        sizeOfSubArray--;
+
+        if(min > tempt_min){
+            min  = tempt_min;
+        }
+    }
+
+    //Find the second smallest number: 
+    for(int i = 0; i<counter; i++){
+        if(minArray[i] == min){
+            continue;
+        }
+        else if(secondMin > minArray[i]){
+            secondMin = minArray[i]; 
+        }
+    }
+    return secondMin; 
+
+}
+
+
+int* readNumbers(){
+    int* array = new int[10]; 
+
+    for(int i = 0; i<10; i++){
+        cin >> array[i] ; 
+    }
+
+    return array; 
+}
+
+void printNumbers(int *numbers, int length){
+    for(int i = 0; i<length; i++){
+        cout << i << " " << numbers[i] << endl; 
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
--- main-4-1.cpp
+++ main-4-1.cpp
@@ -4,10 +4,14 @@ using namespace std;
 
 
 extern int secondSmallestSum(int *numbers,int length); 
+extern int findMinOfSubArray(int *numbers,int length);
+extern int* readNumbers();
+extern void printNumbers(int *numbers, int length);
+
 
 
 int main(){
-    int array[4] = {1,2,3, 4}; 
-    cout << secondSmallestSum(array, sizeof(array)/sizeof(int));
+    int* array = readNumbers();
+    cout << secondSmallestSum(array,10); 
     return 0; 
 }
