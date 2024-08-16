14bca0c2fa342839306f48d30cf6409bda0185a4
Tuan Quan Lam <a1919043@adelaide.edu.au>
2024-08-16 12:26:48 +0930

    sc

--- Person.h
+++ Person.h
@@ -15,19 +15,3 @@ struct PersonList {
 #endif
 
 
-void addItem(string itemName){
-    items[currentItemSpot] = itemName; 
-    currentItemSpot++; 
-}
-
-void addMoney(int money){
-    if(this->money ++ money < maxMoneyCapacity){
-        this->money += money; 
-    }
-}
-
-void printItems(){
-    for(int i = 0; i<currentItemSpot; i++){
-        cout << items[i] << endl; 
-    }
-}
