8afae6fce40aa3b0fc4a8eea51ad4004b732190b
Tuan Quan Lam <a1919043@adelaide.edu.au>
2024-09-02 10:31:55 +0930

    Finish

--- main-1-1.cpp
+++ main-1-1.cpp
@@ -6,9 +6,9 @@ using namespace std;
 int main(){
     Appliance a1(120); 
 
-    cout << a1.get_IsOn() << endl; 
+    cout << a1.get_isOn() << endl; 
     cout << a1.getPowerConsumption() << endl; 
-    cout << a1.get_PowerRating() << endl; 
+    cout << a1.get_powerRating() << endl; 
 
     return 0; 
 }
