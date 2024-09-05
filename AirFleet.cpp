28ed971835c854c7d2087597e8a728a58cf0bf4c
Tuan Quan Lam <a1919043@adelaide.edu.au>
2024-09-05 10:51:31 +0930

    Some changes

--- AirFleet.cpp
+++ AirFleet.cpp
@@ -1,9 +1,15 @@
 #include<iostream>
 using namespace std; 
 #include "AirFleet.h"
+#include "AirVehicle.h"
 
 AirFleet:: AirFleet(){
-    fleet = new AirVehicle*[5]; 
+    fleet = new AirVehicle*[5];
+    fleet[0] = new Airplane(20, 10);
+    fleet[1] = new Helicopter(10000, "BlackHawk");
+    fleet[2] = new AirVehicle(5000);
+    fleet[3] = new Helicopter(100, "WhiteHawk");
+    fleet[4] = new Airplane(15, 20);
 }                           
 /* creates an air fleet containing pointers to 5 AirVehicle objects as an array; 
 */
