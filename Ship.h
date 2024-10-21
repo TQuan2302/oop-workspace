84f4abbd9e47d2373913adeda8e7b0ce6954969f
Tuan Quan Lam <a1919043@adelaide.edu.au>
2024-10-21 20:45:15 +1030

    SC

--- Ship.h
+++ Ship.h
@@ -7,7 +7,7 @@ class Ship: public GameEntity{
     public: 
         void move(int dx, int dy){
             get<0>(position) = dx; 
-            get<0>(position) = dy; 
+            get<1>(position) = dy; 
         }
         Ship(int x, int y): GameEntity(x,y,'n'){
             type = ShipType; 
