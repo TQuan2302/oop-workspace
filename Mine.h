af5078fead372e4e94ae3b708c8913c2a905bfc3
Tuan Quan Lam <a1919043@adelaide.edu.au>
2024-10-23 22:42:55 +1030

    SC

--- Mine.h
+++ Mine.h
@@ -4,16 +4,17 @@
 #include "Explosion.h"
 
 class Mine: public GameEntity{
-    Explosion explode(){
-        this->setType(NoneType); 
-        Explosion explosion(get<0>(this->getPosition()), get<1>(this->getPosition())); 
-        return explosion; 
+    public:
+        Explosion explode(){
+            this->setType(NoneType); 
+            Explosion explosion(get<0>(this->getPosition()), get<1>(this->getPosition())); 
+            return explosion; 
 
-    }
+        }
 
-    Mine(int x, int y): GameEntity(x,y,'c'){
-        this->setType(MineType); 
-    }
+        Mine(int x, int y): GameEntity(x,y,'c'){
+            this->setType(MineType); 
+        }
 };  
 
 #endif
--- Utils.h
+++ Utils.h
@@ -7,7 +7,7 @@
 using namespace std; 
 
 class Utils{
-    static tuple<int, int> generateRandomPos(int gridWidth, int gridHeight){
+    tuple<int, int> generateRandomPos(int gridWidth, int gridHeight){
         //Min is 0, max is gridWidth, gridHeight
         int randomWidth = rand() % gridWidth; 
         int randomHeight = rand() % gridHeight; 
@@ -15,10 +15,24 @@ class Utils{
         tuple<int, int> randomPos(randomWidth, randomHeight); 
         return randomPos; 
     }
-    static double calculateDistance(std::tuple<int, int> pos1, std::tuple<int, int> pos2){
+    double calculateDistance(std::tuple<int, int> pos1, std::tuple<int, int> pos2){
         double result = sqrt( pow( get<0>(pos2) - get<0>(pos1) ,2) + pow(get<1>(pos2) - get<1>(pos1), 2)); 
         return result; 
     }
 };
 
 #endif
+
+tuple<int, int> Utils:: generateRandomPos(int gridWidth, int gridHeight){
+    //Min is 0, max is gridWidth, gridHeight
+    int randomWidth = rand() % gridWidth; 
+    int randomHeight = rand() % gridHeight; 
+        
+    tuple<int, int> randomPos(randomWidth, randomHeight); 
+    return randomPos; 
+}
+
+double Utils:: calculateDistance(std::tuple<int, int> pos1, std::tuple<int, int> pos2){
+        double result = sqrt( pow( get<0>(pos2) - get<0>(pos1) ,2) + pow(get<1>(pos2) - get<1>(pos1), 2)); 
+        return result; 
+    }
